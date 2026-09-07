6939de0f2fbd0d9f684388dbdec2c40a921f2106
KroFFAms <laylojumaeva93@gmail.com>
2026-09-07 13:17:45 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
      - Supabase/API so'rovlari -> keshlanmaydi
    Yangi versiya chiqarganda KESH raqamini oshiring.
    ========================================================== */
-var KESH = 'intizom-v77';
+var KESH = 'intizom-v78';
 
 /* Tarmoqni qancha kutamiz. Bundan uzoq kutish foydasiz:
    keshda ishlaydigan nusxa turibdi. */
