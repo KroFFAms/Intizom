2a6119e46584a0e03486eaaae221830e8826059b
KroFFAms <laylojumaeva93@gmail.com>
2026-09-10 10:26:40 +0500

    Update cache version to intizom-v101
    Increment cache version to v101 for updates.

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
      - Supabase/API so'rovlari -> keshlanmaydi
    Yangi versiya chiqarganda KESH raqamini oshiring.
    ========================================================== */
-var KESH = 'intizom-v100';
+var KESH = 'intizom-v101';
 
 /* Tarmoqni qancha kutamiz. Bundan uzoq kutish foydasiz:
    keshda ishlaydigan nusxa turibdi. */
